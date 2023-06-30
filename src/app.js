8efc161114465cf834abe8092f378b47e1c83ee9
Gisele Macieira <giselefchaves@gmail.com>
2023-06-29 23:58:42 -0300

    endpoints get e post /participants adicionados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import Joi from 'joi';
+import dayjs from 'dayjs';
 
 const app = express();
 
@@ -11,6 +13,13 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
+
+const data = Date.now();
+const hora = dayjs(data).hour();
+const minutos = dayjs(data).minute();
+const segundos = dayjs(data).second();
+const horario = `${hora}:${minutos}:${segundos}`;
+
 mongoClient.connect()
     .then(() => {
         db = mongoClient.db();
@@ -18,17 +27,71 @@ mongoClient.connect()
     })
     .catch((err) => console.log(err.message));
 
-app.post('/teste', async (req, res) => {
-    try{
-        await db.collection("teste").insertOne({
-            nome: "Gisele",
-            sobrenome: "Macieira"
-        })
-        return res.send('Documento adicionado a DB batepapooul e collection teste')
-    }catch (err){
-        return res.status(500).send(err.message)
-    }
-})
+app.post('/participants', async (req, res) => {
+    // Body: { name: "João" }
+    const {name} = req.body;
+    const participantSchema = Joi.object({
+        name: Joi.string().required()
+    });
 
+    const validation = participantSchema.validate(req.body, {abortEarly: false});
+    if(validation.error){
+        //const errors = validation.error.details.map(detail => detail.message);
+        return res.sendStatus(422);
+    }
+    try{
+        const participant = await db.collection("participants").findOne({name: name});
+        if(participant) return res.sendStatus(409);
+
+        // Formato participante: { name: 'xxx', lastStatus: Date.now() }
+        await db.collection("participants").insertOne({
+            name: name,
+            lastStatus: Date.now()
+        });
+        // Formato message: { from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:mm:ss' }
+        await db.collection("messages").insertOne({
+            from: name, 
+            to: 'Todos', 
+            text: 'entra na sala...', 
+            type: 'status', 
+            time: horario
+        })
+        return res.sendStatus(201);
+    }catch (err){
+        return res.status(500).send(err.message);
+    }
+});
+
+app.get('/participants', async (req, res) => {
+    try{
+        const participants = await db.collection('participants').find().toArray();
+        return res.status(201).send(participants);
+    }catch (err){
+        return res.status(500).send(err.message);
+    }
+    
+});
+
+app.post('/messages', async (req, res) => {
+    //Body: { to: "Maria", text: "oi sumida rs", type: "private_message" }
+    const {to, text, type} = req.body;
+    const User = req.headers.user;
+     const messageSchema = Joi.object({
+        to: Joi.string().required(),
+        text: Joi.string().required(),
+        type: Joi.any().valid('message', 'private_message')
+    });
+    const validation = messageSchema.validate(req.body, {abortEarly: false});
+    if(validation.error){
+        //const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send('Erro na validação do body');
+    }
+    if(!User){
+        return res.sendStatus(422);
+    }
+    const participant = await db.collection("participants").findOne({name: User});
+    if(!participant) return res.status(422).send('Participante não encontrado!!');
+    return res.send(`Tudo certo! user: ${User}, to: ${to}, text: ${text}, type: ${type}, participant: ${participant.name}`)
+})
 
 app.listen(5000, () => console.log("Servidor rodando na porta 5000"));
